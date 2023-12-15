82df97c5dea4f474b32af591a96f8d456bacd712
Nestor Arturo Castañeda <nestor8772@gmail.com>
2023-12-14 21:19:35 -0600

    se modifica la estructura basica para poder verificar que solo existan peticiones del tipo GET, verificacion de  existencia de URL y lector de archivos

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,66 @@
 import http from "node:http";
 import fs from "node:fs";
+import path from "node:path";
+ 
+const publicDirectory = path.join(__dirname, "..", "public");
  
 const server = http.createServer((req, res) => {
-  // Verificamos la url
-  if (req.url === "/index.html") {
-    // Leemos el archivo
-    fs.readFile("./public/index.html", (err, content) => {
-      // Manejamos error de lectura
-      if (err) {
-        console.log(err);
+    // Solo aceptamos peticiones GET
+    if (req.method !== "GET") {
+        res.writeHead(405);
+        res.end("Method Not Allowed");
+        return;
+      }
+ // Si no hay URL, devolvemos un error 400
+  if (!req.url) {
+    res.writeHead(400);
+    res.end("Bad Request");
+    return;
+  }
+ // se asigna la ruta del archivo a filePath
+  let filePath = path.join(
+    publicDirectory,
+    req.url === "/" ? "index.html" : req.url
+  );
+    
+  const ext = path.extname(filePath);  //obtenemos la extension del archivo
+  
+  let contentType = "text/html";  //El MIME Type que estamos usando (valor de la cabecera "Content-Type")
+  // siempre es "text/html" pero es posible servir toda clase de archivos. 
+  //Agreguemos soporte a por lo menos las siguientes extensiones: .html, .css, .js, .jpg y jpeg.
+ 
+  switch (ext) {
+    case ".css": {
+      contentType = "text/css";
+      break;
+    }
+    case ".js": {
+      contentType = "text/javascript";
+      break;
+    }
+    case ".jpg":
+    case ".jpeg": {
+      contentType = "image/jpeg";
+      break;
+    }
+  }
+ 
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      console.log(err);
+      if (err.code === "ENOENT") {
+        res.writeHead(404);
+        res.end("Not Found");
+      } else {
         res.writeHead(500);
         res.end("Internal Server Error");
-      } else {
-        // Agregamos la cabecera Content-Type y enviamos el contenido
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(content);
       }
-    });
-  } else {
-    // En caso la url no apunte a /index.html
-    res.writeHead(404);
-    res.end("Not Found");
-  }
+      return;
+    }
+ 
+    res.writeHead(200, { "Content-Type": contentType });
+    res.end(content);
+  });
 });
  
 server.listen(5500, () =>
